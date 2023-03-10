06ee34d542f6d0bc871ded5865b58be511b44a06
Émile Perron <contact@emileperron.com>
2023-03-10 16:21:20 -0500

    test: add test for Product::getChangeset()

--- tests/Model/Ecommerce/ProductTest.php
+++ tests/Model/Ecommerce/ProductTest.php
@@ -20,6 +20,17 @@ class ProductTest extends \PHPUnit\Framework\TestCase
 		$this->assertEquals(self::sampleData(), $this->model->toArray(), "Converts back to an array matching Webflow's expected format.");
 	}
 
+	public function testGetChangeset(): void
+	{
+		$this->model->draft = true;
+		$this->model->setField('color', 'Blue');
+
+		$this->assertEquals([
+			'_draft' => true,
+			'color' => 'Blue',
+		], $this->model->getChangeset());
+	}
+
 	/**
 	 * An array of sample data taken directly from Webflow's API documentation
 	 * for this model.
